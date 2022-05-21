21e016d682b3d066d4694fe8ef9c165909ca88ca
Daniel Burger <48986191+danielburger1337@users.noreply.github.com>
2022-05-21 15:07:00 +0200

    added usefull string data transformers

---
+++ src/Form/DataTransformer/CapitalizeStringDataTransformer.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types=1);
+
+namespace SBSEDV\Bundle\FormBundle\Form\DataTransformer;
+
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+
+/**
+ * @implements DataTransformerInterface<string, string>
+ */
+class CapitalizeStringDataTransformer implements DataTransformerInterface
+{
+    public function __construct(
+        private ?string $encoding = null
+    ) {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function transform(mixed $value): ?string
+    {
+        return $value;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function reverseTransform(mixed $value): ?string
+    {
+        if (null === $value) {
+            return $value;
+        }
+
+        if (!\is_string($value)) {
+            throw new TransformationFailedException('Expected type string.');
+        }
+
+        return $this->mb_ucfirst($value);
+    }
+
+    private function mb_ucfirst(string $string): string
+    {
+        $firstChar = \mb_substr($string, 0, 1, $this->encoding);
+        $lastChars = \mb_substr($string, 1, null, $this->encoding);
+
+        return \mb_strtoupper($firstChar, $this->encoding).$lastChars;
+    }
+}
---
+++ src/Form/DataTransformer/UppercaseStringDataTransformer.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+namespace SBSEDV\Bundle\FormBundle\Form\DataTransformer;
+
+use Symfony\Component\Form\DataTransformerInterface;
+
+/**
+ * @implements DataTransformerInterface<string, string>
+ */
+class UppercaseStringDataTransformer implements DataTransformerInterface
+{
+    public function __construct(
+        private ?string $encoding = null
+    ) {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function transform(mixed $value): ?string
+    {
+        return $value;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function reverseTransform(mixed $value): ?string
+    {
+        if (\is_string($value)) {
+            return \mb_strtoupper($value, $this->encoding);
+        }
+
+        return $value;
+    }
+}
