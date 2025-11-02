69a06a15e3bcf95c7ed58d33bb5692b38c87ac43
Daniel Burger <48986191+danielburger1337@users.noreply.github.com>
2025-11-02 15:50:41 +0100

    Include configs in php-cs-fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -22,7 +22,10 @@ return (new PhpCsFixer\Config())
     ->setRiskyAllowed(true)
     ->setFinder(
         PhpCsFixer\Finder::create()
-            ->in(__DIR__.'/src')
+            ->in([
+                __DIR__.'/src',
+                __DIR__.'/config',
+            ])
             ->append([__FILE__])
     )
 ;
